41f356283cf5b1da80bcd7da346b904b87f0369e
Cesar Costa <cesarcosta_@hotmail.com>
2025-06-17 13:35:14 +0100

    Add project files.

---
+++ DataObjects/ManaboxCsvDTO.cs
@@ -0,0 +1,19 @@
+﻿namespace Mana2DragonShield.DataObjects
+{
+    [Serializable]
+    public class ManaboxCsvDTO
+    {
+        public string? FolderName { get; set; }
+        public string? Quantity { get; set; }
+        public string? TradeQuantity { get; set; }
+        public string? CardName { get; set; }
+        public string? SetCode { get; set; }
+        public string? SetName { get; set; }
+        public string? CardNumber { get; set; }
+        public string? Condition { get; set; }
+        public string? Printing { get; set; }
+        public string? Language { get; set; }
+        public string? PriceBought { get; set; }
+        public string? DateBought { get; set; }
+    }
+}
